7aed0aa279130ee888d599b0406b79915f519575
facugon <facugon@theeye.io>
2021-12-03 17:54:31 -0300

    reset hard

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ then
   exit;
 fi
 
+git reset HEAD --hard
+
 git clone https://github.com/theeye-io-team/theeye-web.git -b development
 git clone https://github.com/theeye-io-team/theeye-supervisor.git -b development
 git clone https://github.com/theeye-io-team/theeye-gateway.git -b development
@@ -12,10 +14,11 @@ git clone https://github.com/theeye-io-team/theeye-gateway.git -b development
 cp -r theeye-web/docs/ dist/theeye-web/
 cp -r theeye-supervisor/docs/ dist/theeye-supervisor/
 cp -r theeye-gateway/docs/ dist/theeye-gateway
-cp -r startup/ dist/
+#cp -r startup/ dist/
 
 sed -i "s|theeye-web|${THEEYE_DOCS_URL}/theeye-web|" dist/_coverpage.md
 sed -i "s|theeye-supervisor|${THEEYE_DOCS_URL}/theeye-supervisor|" dist/_coverpage.md
+#sed -i "s|theeye-gateway|${THEEYE_DOCS_URL}/theeye-gateway|" dist/_coverpage.md
 
 #aws s3 sync dist/ s3://theeye-docs/
 
