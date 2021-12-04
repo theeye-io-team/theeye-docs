a10a908d48840dce869bc1fca08a5bd970432b25
facugon <facugon@theeye.io>
2021-12-04 14:28:07 -0300

    feat: stc structure

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,40 @@ then
   exit;
 fi
 
-git reset HEAD --hard
+#git reset HEAD --hard
 
-git clone https://github.com/theeye-io-team/theeye-web.git -b development
-git clone https://github.com/theeye-io-team/theeye-supervisor.git -b development
-git clone https://github.com/theeye-io-team/theeye-gateway.git -b development
+if [ ! -d theeye-web ];
+then
+  git clone https://github.com/theeye-io-team/theeye-web.git -b development
+else
+  (cd theeye-web; git fetch; git reset HEAD --hard)
+fi
+
+if [ ! -d theeye-supervisor ];
+then
+  git clone https://github.com/theeye-io-team/theeye-supervisor.git -b development
+else
+  (cd theeye-supervisor; git fetch; git reset HEAD --hard)
+fi
+
+if [ ! -d theeye-gateway ];
+then
+  git clone https://github.com/theeye-io-team/theeye-gateway.git -b development
+else
+  (cd theeye-gateway; git fetch; git reset HEAD --hard)
+fi
+
+rm -rf dist
+mkdir dist
+cp -r src/* dist/
 
 cp -r theeye-web/docs/ dist/theeye-web/
 cp -r theeye-supervisor/docs/ dist/theeye-supervisor/
 cp -r theeye-gateway/docs/ dist/theeye-gateway
 #cp -r startup/ dist/
 
-sed -i "s|theeye-web|${THEEYE_DOCS_URL}/theeye-web|" dist/_coverpage.md
-sed -i "s|theeye-supervisor|${THEEYE_DOCS_URL}/theeye-supervisor|" dist/_coverpage.md
+sed -i "s|/theeye-web|${THEEYE_DOCS_URL}/theeye-web|" dist/_coverpage.md
+sed -i "s|/theeye-supervisor|${THEEYE_DOCS_URL}/theeye-supervisor|" dist/_coverpage.md
 #sed -i "s|theeye-gateway|${THEEYE_DOCS_URL}/theeye-gateway|" dist/_coverpage.md
 
 #aws s3 sync dist/ s3://theeye-docs/
---
+++ src/_coverpage.md
@@ -0,0 +1,12 @@
+![logo](./images/logo-theeye-theOeye.png)
+
+# TheEye <small>2.9.0</small>
+
+> An Intelligent Process Automation platform IPA
+
+<!-- [Gateway documentation](https://github.com/theeye-io-team/theeye-gateway) -->
+[Web documentation](/theeye-web)
+[Supervisor documentation](/theeye-supervisor)
+[Getting started](#TheEye)
+
+![color](#f0f0f0)
