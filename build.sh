87885d9342d073521fb56988e14d49e2f94aee24
facugon <facugon@theeye.io>
2022-01-14 17:22:24 -0300

    build and deploy scripts. convertpage updated

---
+++ build.sh
@@ -0,0 +1,48 @@
+#/bin/bash -e
+
+if [ -z "${THEEYE_DOCS_URL+x}" ];
+then
+  echo 'Env THEEYE_DOCS_URL undefined'
+  exit;
+fi
+
+if [ -z "${1+y}" ];
+then
+  branch="development"
+else
+  branch="${1}"
+fi
+echo "using branch ${branch}"
+
+function prepareDocs {
+  echo "-----------------------------------------------------------------"
+  repo=${1}
+
+  if [ ! -d ${repo} ];
+  then
+    git clone https://github.com/theeye-io-team/${repo}.git -b ${branch}
+  else
+    (
+      cd ${repo};
+      echo "in $(pwd); reseting repo";
+      git fetch;
+      git checkout ${branch}
+      git reset HEAD --hard
+    )
+  fi
+
+  cp -r ${repo}/docs/ dist/${repo}/
+
+  sed -i "s|/${repo}|${THEEYE_DOCS_URL}/${repo}|" dist/_coverpage.md
+}
+
+# clean dist
+rm -rf dist
+mkdir dist
+cp -r src/* dist/
+
+prepareDocs theeye-web
+prepareDocs theeye-supervisor
+prepareDocs theeye-gateway
+prepareDocs theeye-agent
+
--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,6 @@
-#/bin/bash -e
 
-if [ -z "${THEEYE_DOCS_URL+x}" ];
-then
-  echo 'Env THEEYE_DOCS_URL undefined'
-  exit;
-fi
+#bash ./build.sh
 
-if [ -z "${1+y}" ];
-then
-  branch="development"
-else
-  branch="${1}"
-fi
-echo "using branch ${branch}"
+aws s3 sync dist/ s3://${1}/
 
-function prepareDocs {
-  echo "-----------------------------------------------------------------"
-  repo=${1}
-
-  if [ ! -d ${repo} ];
-  then
-    git clone https://github.com/theeye-io-team/${repo}.git -b ${branch}
-  else
-    (
-      cd ${repo};
-      echo "in $(pwd); reseting repo";
-      git fetch;
-      git reset HEAD --hard
-    )
-  fi
-
-  cp -r ${repo}/docs/ dist/${repo}/
-
-  sed -i "s|/${repo}|${THEEYE_DOCS_URL}/${repo}|" dist/_coverpage.md
-}
-
-# clean dist
-rm -rf dist
-mkdir dist
-cp -r src/* dist/
-
-prepareDocs theeye-web
-prepareDocs theeye-supervisor
-prepareDocs theeye-gateway
-prepareDocs theeye-agent
-
-#cp -r startup/ dist/
-
-#aws s3 sync dist/ s3://theeye-docs/
-
+aws cloudfront create-invalidation --distribution-id E3K1GG932YLTA7 --paths "/*"
